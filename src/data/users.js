6b0d4131c08a8678f3ac702e159bd385f0a9561d
Javier Lima <javierlimasanchez0@gmail.com>
2026-05-08 19:34:30 +0200

    Test, memoria y usuarios

--- src/data/users.js
+++ src/data/users.js
@@ -4,5 +4,24 @@ export const users = [
         name: 'Javier Lima',
         email: 'jlima@libreria.com',
         password: '1234'
+    },
+    {
+        id: 2,
+        name: 'Admin',
+        email: 'admin@libreria.com',
+        password: 'admin1234'
+    },
+    {
+        id: 3,
+        name: 'Jose Barcena',
+        email: 'jbarcena@libreria.com',
+        password: '1234'
+    },
+    {
+        id: 4,
+        name: 'Miguel Campos',
+        email: 'mcampos@libreria.com',
+        password: '1234'
     }
+
 ]
--- src/test/setup.js
+++ src/test/setup.js
@@ -1,5 +1,20 @@
 import '@testing-library/jest-dom'
 
+const createStorage = () => {
+    const store = {}
+    return {
+        getItem: (key) => store[key] ?? null,
+        setItem: (key, value) => { store[key] = String(value) },
+        removeItem: (key) => { delete store[key] },
+        clear: () => { Object.keys(store).forEach(k => delete store[k]) },
+        get length() { return Object.keys(store).length },
+        key: (n) => Object.keys(store)[n] ?? null,
+    }
+}
+
+global.localStorage = createStorage()
+global.sessionStorage = createStorage()
+
 beforeEach(() => {
     localStorage.clear()
 })
